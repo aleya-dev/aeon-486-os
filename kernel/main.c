b1151db8d66ff93b2983f22eb1ee0a584981de99
Robin Degen <robindegen@gmail.com>
2023-01-22 16:25:29 +0100

    First working version 32-bit

---
+++ kernel/main.c
@@ -0,0 +1,18 @@
+static char* video_memory = (char*) 0xb8000;
+
+void kprint(char *str)
+{
+    char *offset = video_memory;
+    while (*str != '\0')
+    {
+        *offset = *str;
+        ++str;
+        ++offset;
+        ++offset;
+    }
+}
+
+void kernel_main()
+{
+    kprint("Hello from the 32-bit kernel.");
+}
--- make.sh
+++ make.sh
@@ -6,46 +6,20 @@ output_image=floppy.img
 # Build bootloader
 nasm boot/boot.asm -f bin -o ${output_dir}/boot.bin
 
-#grub-mkimage --prefix=/boot --compression=none --format=i386-pc --output=${output_dir}/grub.bin multiboot fat
-#size=$(ls --block-size=512 -s "${output_dir}/grub.bin" | sed 's/\s.*$//')
+# Build kernel
+nasm kernel/entry.asm -f elf -o ${output_dir}/entry.o
+i486-elf-gcc -o2 -ffreestanding -c kernel/main.c -o ${output_dir}/main.o
+i486-elf-ld -o ${output_dir}/kernel.bin -Ttext 0x1000 ${output_dir}/entry.o ${output_dir}/main.o --oformat binary
 
 # Generate the floppy image
 dd if=/dev/zero of=${output_dir}/floppy.img bs=512 count=2880
 
-# Replace the bootsector with the one from grub
-#dd if=${grub_prefix}/lib/grub/i386-pc/boot.img of=${output_dir}/floppy.img conv=notrunc
-#dd if=${output_dir}/boot.bin of=${output_dir}/floppy.img conv=notrunc
+# Write bootsector
+dd if=${output_dir}/boot.bin of=${output_dir}/floppy.img conv=notrunc
 
+# Check kernel size
+kernel_size=$(stat --printf="%s" ${output_dir}/kernel.bin)
+echo "Kernel size: ${kernel_size} bytes"
 
-#grub_bin_size=$(stat --printf="%s" ${output_dir}/grub.bin)
-#size=$(ls --block-size=512 -s "${output_dir}/grub.bin" | sed 's/\s.*$//')
-
-#echo "second img ${size}"
-
-# Add the generated grub image
-#dd if=${output_dir}/grub.bin of=${output_dir}/floppy.img conv=notrunc seek=1
-
-#echo "Mformat blocks $(($size + 2))"
-#mformat -i "${output_dir}/floppy.img" -kR 3
-
-#dd if=${output_dir}/boot.bin of=${output_dir}/floppy.img conv=notrunc
-
-mformat -i "${output_dir}/floppy.img" -B ${output_dir}/boot.bin -kR 3
-
-#echo "mmd"
-#mmd -i "${output_dir}/floppy.img" grub
-
-#mkfs.msdos -F 12 ${output_dir}/floppy.img
-
-
-#grub-install --verbose --allow-floppy --compress=no --target=i386-pc ${output_dir}/floppy.img
-
-# Copy grub
-#grub_bin_size=$(stat --printf="%s" ${output_dir}/grub.bin)
-
-#dd conv=notrunc if=${output_dir}/grub.bin of=${output_dir}/floppy.img bs=${grub_bin_size} count=1
-
-# Copy test.bin right after that
-#dd conv=notrunc if=${output_dir}/test.bin of=${output_dir}/floppy.img seek=512 bs=1 count=23
-
-#dd if=boot.bin of=/dev/sda
+# Copy kernel right after that
+dd conv=notrunc if=${output_dir}/kernel.bin of=${output_dir}/floppy.img seek=512 bs=1 count=${kernel_size}
