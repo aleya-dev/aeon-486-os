77e926380b20976b73aee4f6ac224544988e6e22
Robin Degen <robindegen@gmail.com>
2023-02-08 18:42:23 +0100

    Fixed use of tabs in boot.s

--- kernel/boot.s
+++ kernel/boot.s
@@ -25,11 +25,11 @@ stack_top:
 # are available, as the bootloader might have loaded its multiboot structures or
 # modules there. This lets the bootloader know it must avoid the addresses.
 .section .bss, "aw", @nobits
-	.align 4096
+    .align 4096
 boot_page_directory:
-	.skip 4096
+    .skip 4096
 boot_page_table1:
-	.skip 4096
+    .skip 4096
 # Further page tables may be required if the kernel grows beyond 3 MiB.
 
 # The kernel entry point.
@@ -37,87 +37,87 @@ boot_page_table1:
 .global _start
 .type _start, @function
 _start:
-	# Physical address of boot_page_table1.
-	# TODO: I recall seeing some assembly that used a macro to do the
-	#       conversions to and from physical. Maybe this should be done in this
-	#       code as well?
-	movl $(boot_page_table1 - KERNEL_START_OFFSET), %edi
-	# First address to map is address 0.
-	# TODO: Start at the first kernel page instead. Alternatively map the first
-	#       1 MiB as it can be generally useful, and there's no need to
-	#       specially map the VGA buffer.
-	movl $0, %esi
-	# Map 1023 pages. The 1024th will be the VGA text buffer.
-	movl $1023, %ecx
+    # Physical address of boot_page_table1.
+    # TODO: I recall seeing some assembly that used a macro to do the
+    #       conversions to and from physical. Maybe this should be done in this
+    #       code as well?
+    movl $(boot_page_table1 - KERNEL_START_OFFSET), %edi
+    # First address to map is address 0.
+    # TODO: Start at the first kernel page instead. Alternatively map the first
+    #       1 MiB as it can be generally useful, and there's no need to
+    #       specially map the VGA buffer.
+    movl $0, %esi
+    # Map 1023 pages. The 1024th will be the VGA text buffer.
+    movl $1023, %ecx
 
 1:
-	# Only map the kernel.
-	cmpl $_kernel_start, %esi
-	jl 2f
-	cmpl $(_kernel_end - KERNEL_START_OFFSET), %esi
-	jge 3f
+    # Only map the kernel.
+    cmpl $_kernel_start, %esi
+    jl 2f
+    cmpl $(_kernel_end - KERNEL_START_OFFSET), %esi
+    jge 3f
 
-	# Map physical address as "present, writable". Note that this maps
-	# .text and .rodata as writable. Mind security and map them as non-writable.
-	movl %esi, %edx
-	orl $0x003, %edx
-	movl %edx, (%edi)
+    # Map physical address as "present, writable". Note that this maps
+    # .text and .rodata as writable. Mind security and map them as non-writable.
+    movl %esi, %edx
+    orl $0x003, %edx
+    movl %edx, (%edi)
 
 2:
-	# Size of page is 4096 bytes.
-	addl $4096, %esi
-	# Size of entries in boot_page_table1 is 4 bytes.
-	addl $4, %edi
-	# Loop to the next entry if we haven't finished.
-	loop 1b
+    # Size of page is 4096 bytes.
+    addl $4096, %esi
+    # Size of entries in boot_page_table1 is 4 bytes.
+    addl $4, %edi
+    # Loop to the next entry if we haven't finished.
+    loop 1b
 
 3:
-	# Map VGA video memory to 0xC03FF000 as "present, writable".
-	movl $(0x000B8000 | 0x003), boot_page_table1 - KERNEL_START_OFFSET + 1023 * 4
+    # Map VGA video memory to 0xC03FF000 as "present, writable".
+    movl $(0x000B8000 | 0x003), boot_page_table1 - KERNEL_START_OFFSET + 1023 * 4
 
-	# The page table is used at both page directory entry 0 (virtually from 0x0
-	# to 0x3FFFFF) (thus identity mapping the kernel) and page directory entry
-	# 768 (virtually from 0xC0000000 to 0xC03FFFFF) (thus mapping it in the
-	# higher half). The kernel is identity mapped because enabling paging does
-	# not change the next instruction, which continues to be physical. The CPU
-	# would instead page fault if there was no identity mapping.
+    # The page table is used at both page directory entry 0 (virtually from 0x0
+    # to 0x3FFFFF) (thus identity mapping the kernel) and page directory entry
+    # 768 (virtually from 0xC0000000 to 0xC03FFFFF) (thus mapping it in the
+    # higher half). The kernel is identity mapped because enabling paging does
+    # not change the next instruction, which continues to be physical. The CPU
+    # would instead page fault if there was no identity mapping.
 
-	# Map the page table to both virtual addresses 0x00000000 and 0xC0000000.
-	movl $(boot_page_table1 - KERNEL_START_OFFSET + 0x003), boot_page_directory - KERNEL_START_OFFSET + 0
-	movl $(boot_page_table1 - KERNEL_START_OFFSET + 0x003), boot_page_directory - KERNEL_START_OFFSET + 768 * 4
+    # Map the page table to both virtual addresses 0x00000000 and 0xC0000000.
+    movl $(boot_page_table1 - KERNEL_START_OFFSET + 0x003), boot_page_directory - KERNEL_START_OFFSET + 0
+    movl $(boot_page_table1 - KERNEL_START_OFFSET + 0x003), boot_page_directory - KERNEL_START_OFFSET + 768 * 4
 
-	# Set cr3 to the address of the boot_page_directory.
-	movl $(boot_page_directory - KERNEL_START_OFFSET), %ecx
-	movl %ecx, %cr3
+    # Set cr3 to the address of the boot_page_directory.
+    movl $(boot_page_directory - KERNEL_START_OFFSET), %ecx
+    movl %ecx, %cr3
 
-	# Enable paging and the write-protect bit.
-	movl %cr0, %ecx
-	orl $0x80010000, %ecx
-	movl %ecx, %cr0
+    # Enable paging and the write-protect bit.
+    movl %cr0, %ecx
+    orl $0x80010000, %ecx
+    movl %ecx, %cr0
 
-	# Jump to higher half with an absolute jump. 
-	lea 4f, %ecx
-	jmp *%ecx
+    # Jump to higher half with an absolute jump.
+    lea 4f, %ecx
+    jmp *%ecx
 
 .section .text
 
 4:
-	# At this point, paging is fully set up and enabled.
+    # At this point, paging is fully set up and enabled.
 
-	# Unmap the identity mapping as it is now unnecessary. 
-	movl $0, boot_page_directory + 0
+    # Unmap the identity mapping as it is now unnecessary.
+    movl $0, boot_page_directory + 0
 
-	# Reload crc3 to force a TLB flush so the changes to take effect.
-	movl %cr3, %ecx
-	movl %ecx, %cr3
+    # Reload crc3 to force a TLB flush so the changes to take effect.
+    movl %cr3, %ecx
+    movl %ecx, %cr3
 
-	# Set up the stack.
-	mov $stack_top, %esp
+    # Set up the stack.
+    mov $stack_top, %esp
 
-	# Enter the high-level kernel.
-	call kernel_main
+    # Enter the high-level kernel.
+    call kernel_main
 
-	# Infinite loop if the system has nothing more to do.
-	cli
-1:	hlt
-	jmp 1b
+    # Infinite loop if the system has nothing more to do.
+    cli
+1:      hlt
+    jmp 1b
