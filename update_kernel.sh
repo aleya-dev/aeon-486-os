67bc21c5f01e6a399feb9c4c2b8bd4c6a7c78f13
Robin Degen <robindegen@gmail.com>
2023-02-11 16:54:47 +0100

    Make update_kernel.sh generic without hardcoded paths

--- update_kernel.sh
+++ update_kernel.sh
@@ -1,6 +1,23 @@
-sudo losetup -P /dev/loop0 /mnt/d/Development/aeon_486_os/hdd.img
-sudo mount /dev/loop0p1 /mnt/hdd
-sudo cp /mnt/d/Development/aeon_486_os/build_i486_debug/kernel.bin /mnt/hdd/boot/kernel.bin
+script=$(realpath "$0")
+script_path=$(dirname "$script")
+mount_point=/mnt/hdd
+loop_device=/dev/loop0
+config=$1
+
+if [ -z "$config" ]; then
+    config="debug"
+fi
+
+if [ "$config" != "debug" ] && [ "$config" != "release" ]; then
+    echo "Configuration must be debug or release."
+    exit 1
+fi
+
+echo Using config $config
+
+sudo losetup -P ${loop_device} ${script_path}/hdd.img
+sudo mount ${loop_device}p1 ${mount_point}
+sudo cp ${script_path}/build_i486_${config}/kernel.bin ${mount_point}/boot/kernel.bin
 sudo sync
-sudo umount /mnt/hdd
-sudo losetup -d /dev/loop0
+sudo umount ${mount_point}
+sudo losetup -d ${loop_device}
